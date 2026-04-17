664b90e5335291e2f0ada6b322943de8536e88f2
Angel Perez <perryplatipus4@gmail.com>
2026-04-16 23:53:09 -0700

    doen, finallay

--- Week 13 Hash Table Practice/hashInsertDemo.java
+++ Week 13 Hash Table Practice/hashInsertDemo.java
@@ -1,18 +1,21 @@
 public class hashInsertDemo {
     public static void main(String[] args) {
-        // hash codes are determined by key % 10
+        // hash codes are determined by key % n
         // its then put in the corresponding index of my arraylist, acting as my hashtable
         hashTable table = new hashTable(5);
 
-        table.hashInsert(10, "Apple");
-        table.hashInsert(15, "Banana");
-        table.hashInsert(20, "Cherry");
-        table.hashInsert(7, "Mango");
-        table.hashInsert(12, "Orange");
+        // 10 % 5 = 0 , so bucket zero and so on
+        table.hashInsert(10, "apple");
+        table.hashInsert(15, "banana");
+        table.hashInsert(20, "cherry");
+        // 7 % 5 = 2, into bucket 2
+        table.hashInsert(7, "mango");
+        table.hashInsert(12, "orange");
 
-        table.hashInsert(25, "Updated Banana");
+        //Overwrites banana since its the same key
+        table.hashInsert(15, "Banana, but again");
 
-        System.out.println("Insertion complete!");
-        printTable();
+        System.out.println("Insertion finished");
+        table.printTable();
     }
 }
